1792c3786bb2c8f4d47395ee3687329f25476ba1
Tim Uruski <hey@tim.uru.ski>
2024-01-14 13:08:50 -0700

    Update shell aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -15,3 +15,19 @@ alias ql='qlmanage -p "$@" &> /dev/null'
 alias scheme="racket -l sicp --repl"
 alias tic80="/Applications/tic80.app/Contents/MacOS/tic80"
 alias vi="nvim"
+
+# Aliases to make up for escape key twitch
+alias dit="edit"
+alias g="fg"
+alias it="git"
+alias obs="jobs"
+
+# Ignore test files when searching
+alias rrg="rg --ignore-file ~/.ignore --ignore-file ~/.ignore-tests"
+alias rgg="rg --ignore-file ~/.ignore --ignore-file ~/.ignore-tests"
+alias rrrg="rg --ignore-file ~/.ignore --ignore-file ~/.just-tests"
+alias rggg="rg --ignore-file ~/.ignore --ignore-file ~/.just-tests"
+alias fdd="fd --ignore-file ~/.ignore --ignore-file ~/.ignore-tests"
+alias ffd="fd --ignore-file ~/.ignore --ignore-file ~/.ignore-tests"
+alias fddd="fd --ignore-file ~/.ignore --ignore-file ~/.just-tests"
+alias fffd="fd --ignore-file ~/.ignore --ignore-file ~/.just-tests"
